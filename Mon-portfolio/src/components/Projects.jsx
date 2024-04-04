3a4dce3d80a4738ded61218181433e4edd0c24ac
Pierre Comte <p.s.comte@hotmail.fr>
2024-04-04 11:44:49 +0200

    amélio seo

--- Mon-portfolio/src/components/Projects.jsx
+++ Mon-portfolio/src/components/Projects.jsx
@@ -57,12 +57,7 @@ export const Projects = () => {
               Un échantillon de mes derniers projets réaliser avec HTML, CSS,
               SASS, javascript, React et Redux.
             </p>
-            <Container id="projects-tabs" defaultActiveKey="first">
-              <Nav
-                variant="pills"
-                className="nav-pills mb-5 justify-content-center align-items-center"
-                id="pills-tab"
-              ></Nav>
+            <Container id="projects-tabs">
               <Row>
                 {projects.map((project, index) => {
                   return <ProjectCard key={index} {...project} />;
